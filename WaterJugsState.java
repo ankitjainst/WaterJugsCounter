0f1f63865e64890d3d362db819f68363f082cf0d
Ankit Jain <ankitjainst@gmail.com>
2020-04-02 12:13:42 +0530

    Changed variable from stepsRemaining to operationsCount

--- WaterJugsState.java
+++ WaterJugsState.java
@@ -6,8 +6,8 @@ public class WaterJugsState {
     //the current quantity of water in jugs
     public int a, b;
 
-    //the amount of steps that remain to solve from this state of water jug
-    public int stepsRemaining;
+    //the number of operations that were required to achieve this state
+    public int operationsCount;
 
     //the size of water jugs a and b
     //name clarification ma is max a, mb is maxb
@@ -20,10 +20,10 @@ public class WaterJugsState {
     public String desc;
 
     //constructor to create water jugs state
-    public WaterJugsState(int jugACapacity, int jugBCapacity, int stepsRemaining, WaterJugsState parent, String desc){
+    public WaterJugsState(int jugACapacity, int jugBCapacity, int operationsCount, WaterJugsState parent, String desc){
         a = jugACapacity;
         b = jugBCapacity;
-        this.stepsRemaining = stepsRemaining;
+        this.operationsCount = operationsCount;
         this.parent = parent;
         this.desc = desc;
     }
@@ -36,12 +36,12 @@ public class WaterJugsState {
     //throw away a
     public WaterJugsState move1()
     {
-        return privateCons(0, b, stepsRemaining - 1, "throw away a");
+        return privateCons(0, b, operationsCount + 1, "throw away a");
     }
     //throw away b
     public WaterJugsState move2()
     {
-        return privateCons(a, 0, stepsRemaining - 1, "throw away b");
+        return privateCons(a, 0, operationsCount + 1, "throw away b");
     }
     //put from a into b ... remaining capacity in b is mb - b and precondition is a >= mb - b
     public WaterJugsState move3()
@@ -49,7 +49,7 @@ public class WaterJugsState {
         if(!(a>= mb -b)) return null;
         int newB = mb;
         int newA = a - (mb - b);
-        return privateCons(newA, newB, stepsRemaining - 1,"put from a into b, a >= mb - b");
+        return privateCons(newA, newB, operationsCount + 1,"put from a into b, a >= mb - b");
     }
 
     //put from a into b ... remaining capacity in b is mb - b and another precondition can be when a < mb - b
@@ -58,7 +58,7 @@ public class WaterJugsState {
         if((a>= mb -b)) return null;
         int newB = b + (a);
         int newA = 0;
-        return privateCons(newA, newB, stepsRemaining - 1, "put from a into b, a < mb - b");
+        return privateCons(newA, newB, operationsCount + 1, "put from a into b, a < mb - b");
     }
 
     //put from b into a ... remaining capacity in a is ma - a and precondition is b >= ma - a
@@ -67,7 +67,7 @@ public class WaterJugsState {
         if(!(b >= ma - a)) return null;
         int newA = ma;
         int newB = b - (ma - a);
-        return privateCons(newA, newB, stepsRemaining - 1, "put from b into a, b >= ma - a");
+        return privateCons(newA, newB, operationsCount + 1, "put from b into a, b >= ma - a");
     }
 
     //put from b into a ... remaining capacity in a is ma - a and another precondition can be when b < ma - a
@@ -76,19 +76,19 @@ public class WaterJugsState {
         if((b >= ma - a)) return null;
         int newA = a + (b);
         int newB = 0;
-        return privateCons(newA, newB, stepsRemaining - 1, "put from b into a, b < ma - a");
+        return privateCons(newA, newB, operationsCount + 1, "put from b into a, b < ma - a");
     }
 
     //fill a to full
     public WaterJugsState move7()
     {
-        return privateCons(ma, b, stepsRemaining - 1, "fill a to full");
+        return privateCons(ma, b, operationsCount + 1, "fill a to full");
     }
 
     //fill b to full
     public WaterJugsState move8()
     {
-        return privateCons(a, mb, stepsRemaining - 1, "fill b to full");
+        return privateCons(a, mb, operationsCount + 1, "fill b to full");
     }
 
     //helper function useful to print the intermediate steps required to extract the quantity from water jugs
